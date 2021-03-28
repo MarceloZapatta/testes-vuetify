d68448a01cef25f1ca5e7bf1dc8919e7e86e4043
Marcelo <marcelozapatta0@gmail.com>
2021-03-28 01:18:01 -0300

    first

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mattis
+      lacinia mi, vel dictum lectus porta pellentesque. Phasellus condimentum
+      leo nibh, at molestie urna efficitur a. Suspendisse posuere purus sit amet
+      ante pellentesque malesuada. Ut a sapien lorem. Nulla malesuada tincidunt
+      erat id consectetur. Phasellus gravida metus orci, non aliquam ipsum
+      auctor vitae. Nunc pharetra ipsum in enim tempus malesuada. Donec
+      vulputate enim sit amet porta pretium. Sed eget nisl justo. Nulla congue
+      massa nec orci commodo pharetra.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mattis
+      lacinia mi, vel dictum lectus porta pellentesque. Phasellus condimentum
+      leo nibh, at molestie urna efficitur a. Suspendisse posuere purus sit amet
+      ante pellentesque malesuada. Ut a sapien lorem. Nulla malesuada tincidunt
+      erat id consectetur. Phasellus gravida metus orci, non aliquam ipsum
+      auctor vitae. Nunc pharetra ipsum in enim tempus malesuada. Donec
+      vulputate enim sit amet porta pretium. Sed eget nisl justo. Nulla congue
+      massa nec orci commodo pharetra.
+    </p>
   </div>
 </template>
